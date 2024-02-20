b7db4000c44464fec60ef0c8a5a17f6258647687
Vineet Sawhney <gadgetgeek95@gmail.com>
2024-02-19 21:44:36 -0500

    feat: initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,68 @@
+import { unstable_noStore as noStore } from "next/cache";
+import Link from "next/link";
+
+import { CreatePost } from "~/app/_components/create-post";
+import { getServerAuthSession } from "~/server/auth";
+import { signOut } from "next-auth/react";
+
+import { api } from "~/trpc/server";
+
+export default async function Home() {
+  noStore();
+  const session = await getServerAuthSession();
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+          Welcome to <span className="text-[hsl(280,100%,70%)]">Trakr.</span>
+        </h1>
+
+        <div className="flex flex-col items-center gap-2 p-6">
+          <div className="flex flex-col items-center justify-center gap-8">
+            <p className="text-center text-2xl text-white">
+              {session && <span>Logged in as {session.user?.name}</span>}
+            </p>
+            <div className="flex gap-4">
+              <Link
+                href={session ? "/api/auth/signout" : "/api/auth/signin"}
+                className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
+              >
+                {session ? "Sign out" : "Sign in with Google"}
+              </Link>
+              {session && (
+                <Link
+                  className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
+                  href="/dashboard"
+                >
+                  Go to Dashboard
+                </Link>
+              )}
+            </div>
+          </div>
+        </div>
+
+        <CrudShowcase />
+      </div>
+    </main>
+  );
+}
+
+async function CrudShowcase() {
+  const session = await getServerAuthSession();
+  if (!session?.user) return null;
+
+  const latestPost = await api.post.getLatest.query();
+
+  return (
+    <div className="w-full max-w-xs">
+      {latestPost ? (
+        <p className="truncate">Your most recent post: {latestPost.name}</p>
+      ) : (
+        <p>You have no posts yet.</p>
+      )}
+
+      <CreatePost />
+    </div>
+  );
+}
