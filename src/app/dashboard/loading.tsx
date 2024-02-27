c768ec5aa786e018294a23140bb05b8cc7ad7733
Vineet Sawhney <gadgetgeek95@gmail.com>
2024-02-26 20:45:30 -0500

    fix: changed to dot loaders

--- src/app/dashboard/loading.tsx
+++ src/app/dashboard/loading.tsx
@@ -1,5 +1,9 @@
+import DotLoader from "~/components/dot-loader";
+
 export default function Loading() {
   return (
-    <div className="flex size-full items-center justify-center">Loading..</div>
+    <div className="flex size-full items-center justify-center">
+      <DotLoader dotHeight={10} dotWidth={10} />
+    </div>
   );
 }
