a2f06ff5caf316f00af602ed2a77d212acf471b5
Sudiksha <Sudiksha255@gmail.com>
2024-03-09 05:37:39 +0530

    Added link to privacy-policy , work in progress

---
+++ src/app/privacy-policy/page.tsx
@@ -0,0 +1,98 @@
+export default function TermsOfUse() {
+  return (
+    <div className="mt-[4.5rem] flex w-full flex-col items-center justify-center">
+      <div className="w-full max-w-screen-2xl">
+        <div className=" py-4">
+          <div className="text-3xl ">Privacy Policy</div>
+          <p className="flex w-1/2 py-3">
+            Thank you for using our website. This Privacy Policy explains how we
+            collect, use, and protect information about you when you use our
+            services.
+          </p>
+        </div>
+        <div className="main py-1">
+          <div className="text-3xl ">
+            PLEASE READ THIS PRIVACY POLICY CAREFULLY TO UNDERSTAND HOW WE
+            HANDLE YOUR INFORMATION. IF YOU DO NOT AGREE TO THIS PRIVACY POLICY,
+            PLEASE DO NOT USE THE SERVICES.
+          </div>
+          <p className="flex w-1/2 py-3">
+            This Privacy Policy contains the following sections:{" "}
+          </p>
+          <p className="class">
+            1.The Information We Collect and the Ways We Use It 2.Cookies and
+            Online Analytics 3.Notice About Use of Our Forums and Features
+            4.Aggregate and De-Identified Information 5.Your Choices and Rights
+            6.Third Party Links and Features 7.Children&aposs Privacy 8.How We
+            Protect Your Information 9.Retention of Your Information 10.Changes
+            to Our Privacy Policy.
+          </p>
+        </div>
+
+
+        <div className="main py-20 pl-48"> 
+            <div className="text-3xl ">
+                1. THE INFORMATION WE COLLECT AND THE WAYS WE USE IT
+            </div>
+            <p className="py-3 w-10/12 flex">We obtain information about you through the means discussed below when you use our Services. The information we collect and the purposes for which we use it will depend to some extent on the specific Services you use and how you interact with TKWW. The following section describes the categories of information about you that we collect and how we collect and use such information. See the following section for information regarding the purposes for which we collect information.</p>
+            <p className="size-2"> 1.1. Contact and account registration information, e.g., name, email address, postal address, phone number, wireless device address, account username or screen name, and password</p>
+            <p>Purposes of use
+                -Provide the Services
+                -Communicate with you
+                -Personalize your experience
+                -Secure our Services and users
+                -Fraud prevention and legal purposes</p>
+                <p>Sources of personal information
+                    <p>
+                        -You
+                        -Other users who provide information about you in connection with their event or profile
+                        -Consumer data resellers
+                        -Public records databases
+                        -Conferences and other events
+                        -Our Affiliates</p>
+                    </p>
+
+                    <p>1.2. Demographic and statistical information, e.g., gender, interests, lifestyle information, and hobbies</p>
+<p>
+                        Purposes of use
+                        Communicate with you
+                        Provide the Services
+                        Personalize your experience</p>
+                        <p>Sources of personal information</p>
+                            <p>You
+                            Other users who provide information about you in connection with their event or profile
+                            Consumer data resellers
+                            Our Affiliates
+                            Social media networks, in accordance with your privacy preferences on such services
+                             </p>
+                             
+                    <p>
+                        1.3. Financial and transactional information, e.g., shipping address, credit or debit card number, verification number, and expiration date, and information about your transactions and purchases with us</p>
+<p>
+Purposes of use
+Communicate with you
+Provide the Services
+Secure our Services and users
+Fraud prevention and legal purposes
+ </p>
+<p>Sources of personal information</p>
+<p>You
+Third-party payment processors who collect this information on our behalf and who also have an independent relationship with you
+Third party suppliers and sellers
+                    </p>
+
+
+        <div className="py-20 ">
+          <div className="text-3xl ">2. COOKIES AND ONLINE ANALYTICS</div>
+          <p className="flex w-10/12 py-3">
+            We use a variety of online tracking and analytics tools (e.g.,
+            cookies, flash cookies, pixel tags, and HTML5) to collect and
+            analyze information as you use the Services. Among other things,
+            these technologies allow us to offer you a more tailored experience
+            in the future, by understanding and remembering your particular
+            browsing and usage preferences.
+          </p>
+        </div>
+      </div>
+    </div>
+ </div> );}
--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
     <div className="flex min-h-14 w-full items-center justify-center border-t-[1px] border-primary">
@@ -6,11 +8,15 @@ export default function Footer() {
           <h2 className="w-full text-2xl font-extrabold text-accent">Trakr</h2>
         </div>
         <div className="flex w-full gap-6 md:gap-12">
-          <h4 className="cursor-pointer text-sm underline-offset-2 hover:underline">
+          <h4  className="cursor-pointer text-sm underline-offset-2 hover:underline">
+          
             Terms of use
+           
           </h4>
           <h4 className="cursor-pointer text-sm underline-offset-2 hover:underline">
+          <Link href="/privacy-policy">
             Privacy Policy
+            </Link>
           </h4>
         </div>
       </div>
